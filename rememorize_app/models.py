1112886f8373675fc33682e221e9b4a998d77a14
Dongeon Kim <kde713@gmail.com>
2019-06-14 00:01:44 +0900

    카드 표시 유형 field 추가

--- rememorize_app/models.py
+++ rememorize_app/models.py
@@ -5,6 +5,13 @@ from django.db import models
 
 class MemCard(models.Model):
     REMIND_PHASE = [0, 1, 17, 52, 121, 241, 483]
+    TYPE_CHOICE = (
+        (0, 'random'),
+        (1, 'show_key'),
+        (2, 'show_value'),
+    )
+
+    show_type = models.PositiveSmallIntegerField(choices=TYPE_CHOICE, default=TYPE_CHOICE[0][0], help_text="카드 유형")
 
     key = models.TextField(null=False, help_text="기억카드 key")
     value = models.TextField(null=False, help_text="기억카드 value")
